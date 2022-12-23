3eb4fbf10de79d9511d5d2a0158a5974820a0d5b
xfibben <87906057+xfibben@users.noreply.github.com>
2022-12-23 10:18:50 -0500

    fixing api bugs

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -9,7 +9,7 @@ export default async function handler(req,res){
         case "GET":
             try{
                 const products=await Products.find();
-                res.status(200).json(products);
+                return res.status(200).json(products);
 
             }catch(error){
                 return res.status(500).json({error:error.message});
